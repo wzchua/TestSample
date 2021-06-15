26144a9a030f9788853ac0e3e2890a786822f316
Wei Zheng <13881045+wzchua@users.noreply.github.com>
2021-06-15 19:48:59 +0800

    First input

---
+++ MySite/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+namespace MySite
+{
+    class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+        await Bootstrapper
+            .Factory
+            .CreateWeb(args)
+            .DeployToGitHubPages(
+                "statiqdev",
+                "statiqdev.github.io",
+                Config.FromSetting<string>("GITHUB_TOKEN")
+            )
+            .RunAsync();
+    }
+}
